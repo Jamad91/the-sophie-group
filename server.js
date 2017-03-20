aa9bef4f4294a7a15f94a370e524146fc7e04603
Jimmy <jdicolandrea@gmail.com>
2017-03-20 18:20:08 -0400

    for-sale-ify server

--- server.js
+++ server.js
@@ -9,39 +9,35 @@ app.use(volleyball);
 
 app.use(express.static(__dirname));
 
-const puppies = [{
+const forSale = [{
   id: 1,
-  name: 'Taylor',
-  image: 'https://designerdoginfo.files.wordpress.com/2013/01/puggle-puppy-4.jpg?w=584'
+  title: '742 Evergreen Terrace',
+  image: './public/assets/742_Evergreen_Terrace.png'
 }, {
   id: 2,
-  name: 'Reggie',
-  image: 'http://images.shape.mdpcdn.com/sites/shape.com/files/styles/slide/public/puppy-2_0.jpg'
+  title: '31 Spooner St.',
+  image: './public/assets/31_Spooner_Street.jpg'
 }, {
   id: 3,
-  name: 'Christian',
-  image: 'https://www.askideas.com/media/19/Papillon-Puppy-Looking.jpg'
-}, {
-  id: 4,
-  name: 'Jessie',
-  image: 'http://www.101dogbreeds.com/wp-content/uploads/2015/10/Chi-Spaniel-Puppy-Pictures.jpg'
-}, {
-  id: 5,
-  name: 'Pandora',
-  image: 'http://4.bp.blogspot.com/-3JeIxWBU7bY/UKjIt8lVpCI/AAAAAAAABx8/YM8piSOwczs/s1600/Schipperke-Puppy.jpg'
+  title: '1600 Pennsylvania Ave.',
+  image: './public/assets/1600_Pennsylvania_Avenue.jpg'
+}]
+
+const forRent = [{
+
 }];
 
-app.get('/api/puppies', function (req, res) {
-  res.json(puppies.map(({id, name}) => ({id, name})));
+
+app.get('/api/forsale', function (req, res) {
+  res.json(forSale.map(({id, name}) => ({id, name})));
 });
 
-app.get('/api/puppies/:id', function (req, res) {
-  const aPuppy = puppies.find(p => p.id === Number(req.params.id));
-  if (!aPuppy) res.status(404).end();
-  else res.json(aPuppy);
-});
+// app.get('/api/puppies/:id', function (req, res) {
+//   const aPuppy = puppies.find(p => p.id === Number(req.params.id));
+//   if (!aPuppy) res.status(404).end();
+//   else res.json(aPuppy);
+// });
 
 app.listen(3000, function () {
   console.log('Server listening on port', 3000);
 });
-
