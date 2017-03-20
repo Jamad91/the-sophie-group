e5a3ba46952d99294b483b1d98bb975e4b63839d
Jimmy <jdicolandrea@gmail.com>
2017-03-20 16:51:01 -0400

    add file structure

--- browser/index.js
+++ browser/index.js
@@ -2,11 +2,10 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import store from './store';
 import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider>
     <div className="container flexbox-container">
       <div className="jumbotron">
       </div>
