dfd4c79d33a611558cfa037cc8756ef45dcdac7f
Jimmy <jdicolandrea@gmail.com>
2017-03-20 17:25:35 -0400

    template components

---
+++ browser/components/ForSale.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class ForSale extends Component {
+  render () {
+    <div>
+      <h1>For Sale</h1>
+    </div>
+  }
+}
--- browser/components/Navbar.js
+++ browser/components/Navbar.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
-export default class ForRent extends Component {
+export default class Navbar extends Component {
   render () {
     <div>
-      <h1>For Rent</h1>
+      <h1>The Sophie Group</h1>
     </div>
   }
 }
