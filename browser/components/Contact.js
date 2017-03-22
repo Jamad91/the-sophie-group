87131d3a8ad24b174c6bfb1365bf42338d168a5c
Jimmy <jdicolandrea@gmail.com>
2017-03-21 20:36:35 -0400

    begin contact styling

--- browser/components/Contact.js
+++ browser/components/Contact.js
@@ -5,10 +5,34 @@ export default class Contact extends Component {
     return (
       <div>
         <h1 id="title">Contact</h1>
-          <div className="container flexbox-container">
-            <div className="jumbotron">
+        <div className="container flexbox-container">
+          <div className="jumbotron">
+            <div className="contactEntry">
+              <h3>Chano Rivera</h3>
+              <div className="contactImage">
+                <img id="chanoPic" src="../../public/assets/profile-user.jpg"></img>
+              </div>
+              <div className="contactContent">
+                <strong>Phone:</strong>
+                <p>(631) 835-7293</p>
+                <strong>Email:</strong>
+                <p>wonderingaround38@yahoo.com</p>
+              </div>
+            </div>
+            <div className="contactEntry">
+              <h3>Kelley Rivera</h3>
+              <div className="contactImage">
+                <img id="chanoPic" src="../../public/assets/profile-user.jpg"></img>
+              </div>
+              <div className="contactContent">
+                <strong>Phone:</strong>
+                <p>(631) 835-7293</p>
+                <strong>Email:</strong>
+                <p>wonderingaround38@yahoo.com</p>
+              </div>
             </div>
           </div>
+        </div>
       </div>
     )
   }
