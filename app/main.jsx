5855b0b17992e3e624f3d5df175f1d613d710abc
Jimmy <jdicolandrea@gmail.com>
2017-03-24 18:19:07 -0400

    hook up stylesheet

--- app/main.jsx
+++ app/main.jsx
@@ -31,8 +31,7 @@ const ExampleApp = connect(
 render (
   <Provider store={store}>
     <Router history={browserHistory}>
-      <Navbar />
-      <Route path="/" component={Home}>
+      <Route path="/" component={Navbar}>
         <Route path="home" component={Home} />
       </Route>
       <Route path='*' component={NotFound} />
