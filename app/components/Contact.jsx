443e3113a1ed536e6594de2cdc4d60a91d7b1ad5
Jimmy <jdicolandrea@gmail.com>
2017-05-31 02:28:52 -0400

    add profile pic and comment seeds

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -10,13 +10,13 @@ export default class Contact extends Component {
           <div className="jumbotron contacts">
             <div className="contactEntry">
               <div className="contactImage">
-                <img src="./assets/profile-user.jpg"></img>
+                <img src="./assets/chano.jpg"></img>
               </div>
               <div className="contactInfo">
                 <h2>Chano Rivera</h2>
-                <h4>Phone:</h4>
+                <h4><em>Phone:</em></h4>
                 <h4>(631) 835-7293</h4>
-                <h4>Email:</h4>
+                <h4><em>Email:</em></h4>
                 <h4>chano.rivera@thesophiegroup.com</h4>
                 <div className="contactBio">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
               </div>
@@ -28,9 +28,9 @@ export default class Contact extends Component {
               </div>
               <div className="contactInfo">
                 <h2>Kelley Rivera</h2>
-                <h4>Phone:</h4>
+                <h4><em>Phone:</em></h4>
                 <h4>(631) 835-7293</h4>
-                <h4>Email:</h4>
+                <h4><em>Email:</em></h4>
                 <h4>kelley.rivera@thesophiegroup.com</h4>
                 <div className="contactBio">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
               </div>
