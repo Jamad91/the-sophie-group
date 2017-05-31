43ac294bbd06969a52ccd0476c3d604ed340e847
Jimmy <jdicolandrea@gmail.com>
2017-05-31 02:38:17 -0400

    fix bio content

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -18,7 +18,7 @@ export default class Contact extends Component {
                 <h4>(631) 835-7293</h4>
                 <h4><em>Email:</em></h4>
                 <h4>chano.rivera@thesophiegroup.com</h4>
-                <div className="contactBio">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                <div className="contactBio">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
               </div>
             </div>
             <br />
@@ -32,7 +32,7 @@ export default class Contact extends Component {
                 <h4>(631) 835-7293</h4>
                 <h4><em>Email:</em></h4>
                 <h4>kelley.rivera@thesophiegroup.com</h4>
-                <div className="contactBio">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                <div className="contactBio">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
               </div>
             </div>
             <hr />
